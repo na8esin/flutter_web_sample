d644412eb3af150d3b6bf11c631fc91cbe99e20d
na8esin <na8esin.mpst@gmail.com>
2020-07-31 14:01:09 +0900

    get-started/codelab-web Step 2 finished

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_web_sample/main.dart';
+import 'package:flutter_web_sample/myapp.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
