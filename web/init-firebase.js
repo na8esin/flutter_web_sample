38bdf9bf7eebe103591713bf2a39725739f8e9db
na8esin <na8esin.mpst@gmail.com>
2020-08-15 13:30:56 +0900

    firebase initできたところ

---
+++ web/init-firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDpX3qE01diKcqPeVvOol2jrr2eUbd0Tm8",
+  authDomain: "maiasa8zinihoikuennnitodoketai.firebaseapp.com",
+  databaseURL: "https://maiasa8zinihoikuennnitodoketai.firebaseio.com",
+  projectId: "maiasa8zinihoikuennnitodoketai",
+  storageBucket: "maiasa8zinihoikuennnitodoketai.appspot.com",
+  messagingSenderId: "910107858644",
+  appId: "1:910107858644:web:509f4a4f2a6bb5d9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
