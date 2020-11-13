1e8e0380eb0a2a12f5684730be776f85f36c1fc3
t.watanabe <t.watanabe@ifcreate.co.jp>
2020-11-13 16:44:32 +0900

    rename myapp counter_sample_main: mainが入ってるファイルをわかりやすくする

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_web_sample/myapp.dart';
+import 'package:flutter_web_sample/counter_sample_main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
